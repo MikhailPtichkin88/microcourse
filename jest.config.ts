a88c3a38395b52ec16708f2f8893c75d239de119
mptichkin <apple@mptichkin.local>
2024-11-21 22:15:20 +0300

    fix(ci): fix jest

--- jest.config.ts
+++ jest.config.ts
@@ -10,7 +10,7 @@ const createJestConfig = nextJest({
 const config: Config = {
   coverageProvider: 'v8',
   testEnvironment: 'jsdom',
-
+  modulePathIgnorePatterns: ["<rootDir>/tests/"],
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   moduleNameMapper: {
     "^@/(.*)$": "<rootDir>/src/$1",
