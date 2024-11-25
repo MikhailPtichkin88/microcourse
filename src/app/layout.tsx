f7e8d9d937ce50cc13d1cc14b8d522c611a43281
mptichkin <apple@mptichkin.local>
2024-11-25 07:14:17 +0300

    fix: change bg-color

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={cn(`min-h-screen bg-background font-sans antialiased bg-orange-100`, fontSans.variable)}
+        className={cn(`min-h-screen bg-background font-sans antialiased bg-green-200`, fontSans.variable)}
       >
         {children}
       </body>
