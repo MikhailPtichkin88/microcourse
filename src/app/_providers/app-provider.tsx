053420ca590b5ec3643ee40a094394f49c4da32b
mptichkin <apple@MacBook-Pro-Mike.local>
2024-12-11 22:47:11 +0300

    feature: refactor app, add helper functions

---
+++ src/app/_providers/app-provider.tsx
@@ -0,0 +1,12 @@
+"use client"
+import { ThemeProvider } from "@/features/theme/theme-provider";
+import { ComposeChildren } from "@/shared/lib/react";
+import { ReactNode } from "react";
+
+export const AppProvider = ({ children }: { children: ReactNode }) => {
+  return (
+    <ComposeChildren>
+      <ThemeProvider>{children}</ThemeProvider>
+    </ComposeChildren>
+  );
+};
